60a68ba0e7c6fb9bbd4b622250c34379a7a401bb
MatiasRS <matiasrscornik@gmail.com>
2026-06-15 20:59:59 -0300

    feat: enhance Cypress tests and CI workflow

--- cypress/e2e/deployment.cy.js
+++ cypress/e2e/deployment.cy.js
@@ -38,4 +38,32 @@ describe('Despliegue de la calculadora', () => {
       'Resultados: Se deben ingresar dos numeros validos',
     );
   });
+
+  it('muestra error con inputs vacios', () => {
+    cy.get('#concatButton').click();
+
+    cy.get('#result').should(
+      'have.text',
+      'Resultados: Se deben ingresar dos numeros validos',
+    );
+  });
+
+  it('actualiza el resultado en llamadas sucesivas', () => {
+    cy.get('#number1').type('1');
+    cy.get('#number2').type('2');
+    cy.get('#concatButton').click();
+    cy.get('#result').should('have.text', 'Resultados: 12');
+
+    cy.get('#number1').clear().type('5');
+    cy.get('#concatButton').click();
+    cy.get('#result').should('have.text', 'Resultados: 52');
+  });
+
+  it('ignora espacios alrededor en el browser', () => {
+    cy.get('#number1').type('  12  ');
+    cy.get('#number2').type('  34  ');
+    cy.get('#concatButton').click();
+
+    cy.get('#result').should('have.text', 'Resultados: 1234');
+  });
 });
